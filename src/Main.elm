166b34108ac9733de0a24f535520024491074900
Laurens <laurens.decock@odisee.be>
2021-02-19 15:00:29 +0100

    💄 Set up color & font

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Browser
 import Element as Ui
+import Element.Font as Font
 import Html exposing (Html, text)
 import Material.Icons.Communication
 import Widget
@@ -57,9 +58,14 @@ update _ model =
 
 view : Model -> Html Msg
 view model =
-    Ui.layout []
+    Ui.layout
+        [ Font.family
+            [ Font.external { url = "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap", name = "Roboto" }
+            ]
+        , Font.color <| color Text
+        ]
         (Ui.column
-            []
+            [ Ui.width (Ui.fill |> Ui.maximum 1200), Ui.centerX ]
             [ Ui.text "hi"
             , Widget.button (Widget.Material.containedButton Widget.Material.darkPalette)
                 { text = "Submit"
@@ -77,3 +83,22 @@ view model =
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
+
+
+type AppColor
+    = Text
+    | TextInverted
+    | Bg
+
+
+color : AppColor -> Ui.Color
+color col =
+    case col of
+        Text ->
+            Ui.rgb255 0xEE 0xEE 0xEE
+
+        TextInverted ->
+            Ui.rgb255 0x33 0x33 0x33
+
+        Bg ->
+            Ui.rgb255 0x22 0x28 0x31
