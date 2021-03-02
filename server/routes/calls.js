a929b3cd6bb94dc09d4128ae89b73a7775e71e15
Laurens <laurens.decock@odisee.be>
2021-03-02 11:16:01 +0100

    ✨ Pass subTasks with call on adding

--- server/routes/calls.js
+++ server/routes/calls.js
@@ -37,11 +37,19 @@ router.get('/', async (req, res) => {
 
 /**
  * Adds call to db
- * @param {object} call - { who: String, comments: String, when: Number (millisFromPosix) }
+ * @param {object} call - {
+ *  who: String,
+ *  comments: String,
+ *  when: Number (millisFromPosix),
+ *
+ *  subTasks?: {
+ *    text: String,
+ *    done?: Bool
+ *  }
+ * }
  */
 router.put('/add', async (req, res) => {
   const call = req.body.call;
-  console.log(req.body);
 
   if (!call) {
     return res.status(400).json({
@@ -68,16 +76,38 @@ router.put('/add', async (req, res) => {
   }
 
   try {
-    const sql = `
+    const insertCallSQL = `
     INSERT INTO calls (who, comments, created_at, is_archived)
-      VALUES ($1, $2, $3, FALSE);
+      VALUES ($1, $2, $3, FALSE)
+      RETURNING id;
     `;
 
-    await h.db.query(sql, [call.who, call.comments, new Date(call.when)]);
+    const callResult = await h.db.query(insertCallSQL, [
+      call.who,
+      call.comments,
+      new Date(call.when),
+    ]);
+
+    const callId = h.db.sqlToArr(callResult)[0].id;
+    const subTasks = getParsedSubTasks(callId, call.subTasks);
+
+    console.log(callId);
+    console.log(subTasks);
+
+    const prepSubtaskInserts = h.db.prepareInserts(subTasks);
+
+    let insertSubTasksSQL = `
+    INSERT INTO subtasks (call_id, text, done)
+      VALUES 
+    ${prepSubtaskInserts.values}
+    ;     
+    `;
+
+    await h.db.query(insertSubTasksSQL, prepSubtaskInserts.params);
 
     return res.status(200).json({
       message: 'Added call to DB',
-      call,
+      callId,
     });
   } catch (err) {
     h.print.err(err);
@@ -87,5 +117,37 @@ router.put('/add', async (req, res) => {
     });
   }
 });
+
+/**
+ * Returns array of type checked & parsed subtasks
+ * @param {object[]} subTasks
+ */
+function getParsedSubTasks(callId, subTasks) {
+  if (subTasks && Array.isArray(subTasks)) {
+    return subTasks
+      .map((st) => {
+        // subTasks.text is present?
+        if ('text' in st) {
+          const text = String(st.text);
+
+          // subTasks.done is present & is bool?
+          if ('done' in st && typeof st.done === 'boolean') {
+            return {
+              call_id: callId,
+              text,
+              done: st.done,
+            };
+          } else {
+            return {
+              call_id: callId,
+              text,
+              done: false,
+            };
+          }
+        }
+      })
+      .filter((st) => st !== null && st !== undefined);
+  } else return [];
+}
 
 module.exports = router;
