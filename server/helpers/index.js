9a93c568c4d8e513986c5218dfcf3b645f613272
Laurens <laurens.decock@odisee.be>
2021-03-02 11:15:52 +0100

    ✨ prepareInserts helper function

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -29,6 +29,62 @@ module.exports = {
       if (sqlResult && sqlResult.rows && Array.isArray(sqlResult.rows)) return sqlResult.rows;
       else return [];
     },
+
+    /**
+     *  Use it like this:
+     *
+     *  `node-postgres.query('INSERT INTO table (col1, col2, col3)`
+     *
+     *  `VALUES' + `_values_, _params_`);`
+     *
+     * @param {object[]} objects - These should all be structured the same!
+     *  e.g. ```[{ col1: 'hi', col2: true, col3: 5 }, { col1, 'yes', col2: false, col3: 10 }]```
+     *
+     * @returns {object} ``` { values: String, params: [] } ```
+     */
+    prepareInserts: (objects) => {
+      if (!Array.isArray(objects) || !(objects.length > 0)) {
+        return;
+      }
+
+      // the actual string to be inserted in the VALUES ( ... ) query
+      let values = '';
+
+      // this counter keeps track of the correct $int to insert
+      let valueCounter = 1;
+
+      // Take first object as example for structure
+      let objectKeyCount = Object.keys(objects[0]).length;
+
+      for (let i = 0; i < objects.length; i++) {
+        // INSERT VALUES ($1, $2), ($3, $4) and so on ...
+        values += '(';
+
+        for (let j = 0; j < objectKeyCount; j++) {
+          values += '$' + (valueCounter + j);
+          if (j < objectKeyCount - 1) values += ', ';
+        }
+
+        values += ')';
+
+        // next pair should start at this number
+        valueCounter += objectKeyCount;
+
+        // Last one shouldn't have a trailing comma
+        if (i < objects.length - 1) values += ',\n';
+      }
+
+      // bump everything into one consecutive array
+      // ['hi', true, 5, 'yes', false, 10]
+      const params = [];
+      objects.forEach((a) => {
+        for (const key in a) {
+          if (a.hasOwnProperty(key)) params.push(a[key]);
+        }
+      });
+
+      return { values, params };
+    },
   },
 
   /**
